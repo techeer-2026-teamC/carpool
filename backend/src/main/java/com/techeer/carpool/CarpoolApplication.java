80efa0b7a7af72cb6138c681ebf2f3df6dbfcea5
Sua <140071879+ssuasu@users.noreply.github.com>
2026-03-31 11:38:30 +0900

    🎨 Ui 수정 (gitmoji 테스트)
    이런 내용을 수정하였음

--- backend/src/main/java/com/techeer/carpool/CarpoolApplication.java
+++ backend/src/main/java/com/techeer/carpool/CarpoolApplication.java
@@ -3,7 +3,7 @@ package com.techeer.carpool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-
+//안뇽
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CarpoolApplication {
 
