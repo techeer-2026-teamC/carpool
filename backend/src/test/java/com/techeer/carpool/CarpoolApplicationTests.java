407bc55cd8cd2cffae960f6e0eb14a01f136c5ba
wnswp1122 <wnswp1122@naver.com>
2026-04-01 17:37:35 +0900

    fix : 테스트 클래스에 H2 설정 직접 주입으로 CI 빌드 수정

--- backend/src/test/java/com/techeer/carpool/CarpoolApplicationTests.java
+++ backend/src/test/java/com/techeer/carpool/CarpoolApplicationTests.java
@@ -3,7 +3,15 @@ package com.techeer.carpool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.security.oauth2.client.registration.google.client-id=test-id",
+		"spring.security.oauth2.client.registration.google.client-secret=test-secret"
+})
 class CarpoolApplicationTests {
 
 	@Test
